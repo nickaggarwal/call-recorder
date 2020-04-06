fcaf9e9c97cccdb4917434de44ad28e21e2d8ca6
Nilesh <nick.aggarwal@gmail.com>
2020-04-06 09:32:38 +0530

    Removed Auth

--- cjapp/settings.py
+++ cjapp/settings.py
@@ -108,9 +108,6 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-        ),
     'DEFAULT_FILTER_BACKENDS': (
             'django_filters.rest_framework.DjangoFilterBackend'
         )
